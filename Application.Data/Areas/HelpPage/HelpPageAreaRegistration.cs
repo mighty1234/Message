3425340369def1755d027bb3d0392c107f1b5549
mighty1234 <Vladlegenda97@gmail.com>
2018-09-07 15:54:01 +0300

    end sending development

--- Application.Data/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ Application.Data/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -16,7 +16,7 @@ namespace Application.Data.Areas.HelpPage
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
-                "HelpPage_Default",
+                "HelpPage_Def",
                 "Help/{action}/{apiId}",
                 new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
 
