e823fbd4348d8233d6e3d93daec38ea0f2b6efd3
mighty1234 <Vladlegenda97@gmail.com>
2018-09-07 14:09:23 +0300

    add basic controller

--- ApplicationWebApi/Controllers/MessageController.cs
+++ ApplicationWebApi/Controllers/MessageController.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using Application.Data;
+using Application.Service.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
 namespace ApplicationWebApi.Controllers
 {
     public class MessageController : ApiController
     {
-        
+        private IMessageService messageService = new MessageService();
+
+        [Route("api/message/getall")]
+        public IEnumerable<Message> GetAll()
+        {
+            return messageService.GetMessage();
+        }
+        //[Route("api/message/Save")]
+        //public bool SaveMessage(Message mes)
+        //{
+
+        //}
     }
 }
